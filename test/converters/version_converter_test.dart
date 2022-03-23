a32010bde7766af726beb1afc8f0dd738e1478ab
Klemen Tušar <techouse@gmail.com>
2022-03-23 03:09:29 +0000

    :white_check_mark: Add version_converter test

---
+++ test/converters/version_converter_test.dart
@@ -0,0 +1,42 @@
+import 'package:alfred_workflow/src/converters/version_converter.dart';
+import 'package:faker/faker.dart';
+import 'package:pub_semver/pub_semver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  final Faker faker = Faker();
+
+  test('fromJson', () {
+    final int major = faker.randomGenerator.integer(99, min: 0);
+    final int minor = faker.randomGenerator.integer(99, min: 0);
+    final int patch = faker.randomGenerator.integer(99, min: 1);
+
+    expect(
+      VersionConverter.instance.fromJson('$major.$minor.$patch'),
+      Version(major, minor, patch),
+    );
+    expect(
+      VersionConverter.instance.fromJson('v$major.$minor.$patch'),
+      Version(major, minor, patch),
+    );
+    expect(
+      VersionConverter.instance.fromJson('V$major.$minor.$patch'),
+      Version(major, minor, patch),
+    );
+
+    // exceptions
+    expect(
+      () => VersionConverter.instance.fromJson('$major'),
+      throwsA(TypeMatcher<FormatException>()),
+    );
+    expect(
+      () => VersionConverter.instance.fromJson('$major.$minor'),
+      throwsA(TypeMatcher<FormatException>()),
+    );
+    expect(
+      () => VersionConverter.instance
+          .fromJson('${faker.randomGenerator.string(1)}$major.$minor.$patch'),
+      throwsA(TypeMatcher<FormatException>()),
+    );
+  });
+}
