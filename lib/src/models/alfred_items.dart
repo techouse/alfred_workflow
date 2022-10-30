b56e444b7eddb14a35141d28a24af9e1a3c5703c
Klemen Tušar <techouse@gmail.com>
2022-10-30 10:16:35 +0000

    :recycle: Refactor AlfredItems

--- lib/src/models/alfred_items.dart
+++ lib/src/models/alfred_items.dart
@@ -1,29 +1,25 @@
-import 'package:autoequal/autoequal.dart';
 import 'package:equatable/equatable.dart' show EquatableMixin;
-import 'package:json_annotation/json_annotation.dart'
-    show JsonSerializable, JsonKey;
 
 import 'alfred_item.dart';
 
-part 'alfred_items.g.dart';
-
-@Autoequal(mixin: true)
-@JsonSerializable(explicitToJson: true)
-class AlfredItems with EquatableMixin, _$AlfredItemsAutoequalMixin {
-  AlfredItems(this.items);
+class AlfredItems with EquatableMixin {
+  const AlfredItems(this.items);
 
   /// A list of zero or more [AlfredItem]s.
   ///
   /// Each [AlfredItem] describes a result row displayed in Alfred.
-  @JsonKey(fromJson: _itemsFromJson)
-  List<AlfredItem> items;
+  final List<AlfredItem> items;
 
-  static List<AlfredItem> _itemsFromJson(List<dynamic> items) => items
-      .map((e) => AlfredItem.fromJson(Map<String, dynamic>.from(e)))
-      .toList();
+  factory AlfredItems.fromJson(Map<String, dynamic> json) => AlfredItems(
+        (json['items'] as List)
+            .map((e) => AlfredItem.fromJson(Map<String, dynamic>.from(e)))
+            .toList(),
+      );
 
-  factory AlfredItems.fromJson(Map<String, dynamic> json) =>
-      _$AlfredItemsFromJson(json);
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'items': items.map((AlfredItem item) => item.toJson()).toList(),
+      };
 
-  Map<String, dynamic> toJson() => _$AlfredItemsToJson(this);
+  @override
+  List<Object?> get props => [items];
 }
