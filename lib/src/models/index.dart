b49b3689a2d7e3768c28e6071051970dfdc03e68
Klemen Tušar <techouse@gmail.com>
2022-12-18 09:03:41 +0000

    :construction: Add mods

--- lib/src/models/index.dart
+++ lib/src/models/index.dart
@@ -1,6 +1,7 @@
 export 'alfred_item.dart' show AlfredItem;
 export 'alfred_item_icon.dart' show AlfredItemIcon, AlfredItemIconType;
 export 'alfred_item_text.dart' show AlfredItemText;
+export 'alfred_item_mod.dart' show AlfredItemMod, AlfredItemModKey;
 export 'alfred_items.dart' show AlfredItems;
 export 'github_asset.dart' show GithubAsset;
 export 'github_release.dart' show GithubRelease;
