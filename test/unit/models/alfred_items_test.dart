840bddccad0de0fafdc7f824e2264e3fb22108cd
Klemen Tušar <techouse@gmail.com>
2022-12-22 15:55:24 +0000

    :sparkles: Add support result ordering

--- test/unit/models/alfred_items_test.dart
+++ test/unit/models/alfred_items_test.dart
@@ -16,64 +16,82 @@ void main() {
   });
 
   group('toJson', () {
-    test('by default the JSON includes UID and is without skipknowledge', () {
-      items = AlfredItems(itemsList);
-      final Map<String, dynamic> json = items.toJson();
+    test(
+      'by default the JSON includes UID and is without skipknowledge',
+      () {
+        items = AlfredItems(itemsList);
+        final Map<String, dynamic> json = items.toJson();
 
-      expect(json['skipknowledge'], isNull);
-      for (final Map<String, dynamic> item in json['items'] as List) {
-        expect(item['uid'], isNotNull);
-      }
-    });
+        expect(json['skipknowledge'], isNull);
+        for (final Map<String, dynamic> item in json['items'] as List) {
+          expect(item['uid'], isNotNull);
+        }
+      },
+    );
 
-    test('skipKnowledge = true includes skipknowledge', () {
-      items = AlfredItems(itemsList, skipKnowledge: true);
-      final Map<String, dynamic> json = items.toJson();
+    test(
+      'skipKnowledge = true includes skipknowledge',
+      () {
+        items = AlfredItems(itemsList, skipKnowledge: true);
+        final Map<String, dynamic> json = items.toJson();
 
-      expect(json['skipknowledge'], isTrue);
-      for (final Map<String, dynamic> item in json['items'] as List) {
-        expect(item['uid'], isNotNull);
-      }
-    });
+        expect(json['skipknowledge'], isTrue);
+        for (final Map<String, dynamic> item in json['items'] as List) {
+          expect(item['uid'], isNotNull);
+        }
+      },
+    );
 
-    test('skipKnowledge = false includes skipknowledge', () {
-      items = AlfredItems(itemsList, skipKnowledge: false);
-      final Map<String, dynamic> json = items.toJson();
+    test(
+      'skipKnowledge = false includes skipknowledge',
+      () {
+        items = AlfredItems(itemsList, skipKnowledge: false);
+        final Map<String, dynamic> json = items.toJson();
 
-      expect(json['skipknowledge'], isFalse);
-      for (final Map<String, dynamic> item in json['items'] as List) {
-        expect(item['uid'], isNotNull);
-      }
-    });
+        expect(json['skipknowledge'], isFalse);
+        for (final Map<String, dynamic> item in json['items'] as List) {
+          expect(item['uid'], isNotNull);
+        }
+      },
+    );
 
-    test('exactOrder = true removes UIDs', () {
-      items = AlfredItems(itemsList, exactOrder: true);
-      final Map<String, dynamic> json = items.toJson();
+    test(
+      'exactOrder = true removes UIDs',
+      () {
+        items = AlfredItems(itemsList, exactOrder: true);
+        final Map<String, dynamic> json = items.toJson();
 
-      expect(json['skipknowledge'], isNull);
-      for (final Map<String, dynamic> item in json['items'] as List) {
-        expect(item['uid'], isNull);
-      }
-    });
+        expect(json['skipknowledge'], isNull);
+        for (final Map<String, dynamic> item in json['items'] as List) {
+          expect(item['uid'], isNull);
+        }
+      },
+    );
 
-    test('exactOrder = true && skipKnowledge = true removes UIDs and includes skipknowledge', () {
-      items = AlfredItems(itemsList, skipKnowledge: true, exactOrder: true);
-      final Map<String, dynamic> json = items.toJson();
+    test(
+      'exactOrder = true && skipKnowledge = true removes UIDs and includes skipknowledge',
+      () {
+        items = AlfredItems(itemsList, skipKnowledge: true, exactOrder: true);
+        final Map<String, dynamic> json = items.toJson();
 
-      expect(json['skipknowledge'], isTrue);
-      for (final Map<String, dynamic> item in json['items'] as List) {
-        expect(item['uid'], isNull);
-      }
-    });
+        expect(json['skipknowledge'], isTrue);
+        for (final Map<String, dynamic> item in json['items'] as List) {
+          expect(item['uid'], isNull);
+        }
+      },
+    );
 
-    test('exactOrder = true && skipKnowledge = false removes UIDs and includes skipknowledge', () {
-      items = AlfredItems(itemsList, skipKnowledge: false, exactOrder: true);
-      final Map<String, dynamic> json = items.toJson();
+    test(
+      'exactOrder = true && skipKnowledge = false removes UIDs and includes skipknowledge',
+      () {
+        items = AlfredItems(itemsList, skipKnowledge: false, exactOrder: true);
+        final Map<String, dynamic> json = items.toJson();
 
-      expect(json['skipknowledge'], isFalse);
-      for (final Map<String, dynamic> item in json['items'] as List) {
-        expect(item['uid'], isNull);
-      }
-    });
+        expect(json['skipknowledge'], isFalse);
+        for (final Map<String, dynamic> item in json['items'] as List) {
+          expect(item['uid'], isNull);
+        }
+      },
+    );
   });
 }
