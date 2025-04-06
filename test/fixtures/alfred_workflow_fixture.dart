f35ee77f4513d88de323f5a43507ad60e4951271
Klemen Tušar <techouse@gmail.com>
2025-04-06 10:45:25 +0100

    :white_check_mark: add support for automatic and file caching in AlfredWorkflowFixture

--- test/fixtures/alfred_workflow_fixture.dart
+++ test/fixtures/alfred_workflow_fixture.dart
@@ -3,6 +3,7 @@ import 'package:data_fixture_dart/data_fixture_dart.dart';
 import 'package:meta/meta.dart';
 
 import '../helpers/mock_alfred_cache.dart';
+import 'models/alfred_automatic_cache_fixture.dart';
 
 extension AlfredWorkflowFixture on AlfredWorkflow {
   static AlfredWorkflowFactory get factory => AlfredWorkflowFactory();
@@ -11,14 +12,27 @@ extension AlfredWorkflowFixture on AlfredWorkflow {
 @internal
 final class AlfredWorkflowFactory extends FixtureFactory<AlfredWorkflow> {
   @override
-  FixtureDefinition<AlfredWorkflow> definition() => define(
-        (Faker faker) => AlfredWorkflow(
-          cache: MockAlfredCache<AlfredItems>(
-            fromEncodable: (Map<String, dynamic> json) =>
-                AlfredItems.fromJson(json),
-          ),
-        ),
-      );
+  FixtureDefinition<AlfredWorkflow> definition() =>
+      define((Faker faker) => AlfredWorkflow());
+
+  FixtureRedefinitionBuilder<AlfredWorkflow> withAutomaticCache(
+    AlfredAutomaticCache? automaticCache,
+  ) =>
+      (_) => AlfredWorkflow(
+            automaticCache: automaticCache ??
+                AlfredAutomaticCacheFixture.factory.makeSingle(),
+          );
+
+  FixtureRedefinitionBuilder<AlfredWorkflow> withFileCache(
+    AlfredCache<AlfredItems>? fileCache,
+  ) =>
+      (_) => AlfredWorkflow(
+            fileCache: fileCache ??
+                MockAlfredCache<AlfredItems>(
+                  fromEncodable: (Map<String, dynamic> json) =>
+                      AlfredItems.fromJson(json),
+                ),
+          );
 
   FixtureRedefinitionBuilder<AlfredWorkflow>
       withoutAlfredSmartResultOrdering() => (AlfredWorkflow workflow) =>
